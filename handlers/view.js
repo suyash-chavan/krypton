f8f480925640dc382643713de976ac8287166681
Suyash Chavan <suyashc222@gmail.com>
2021-06-22 00:54:59 +0530

    view enrolled pending

--- handlers/view.js
+++ handlers/view.js
@@ -1,6 +1,6 @@
 const luxon = require('luxon');
 const DATE_FORMAT = 'yyyy-MM-dd';
-const TIME_FORMAT = 'hh:mm';
+const prefix = "$";
 const sendRequest = require('../helpers/sendRequest.js');
 
 function dateTimeToDateString(dateTime) {
@@ -29,8 +29,8 @@ async function getAssignments(classID) {
 
     return response.data;
 }
-async function handleView(message) {
 
+async function handleAssignments(message) {
     var classID = message.channel.id;
 
     var assignments = await getAssignments(classID);
@@ -59,6 +59,174 @@ async function handleView(message) {
             ]
         }
     });
+}
+
+async function getReport(assNo, classID) {
+    classID = "id_" + classID;
+
+    var data = JSON.stringify({
+        "operation": "sql",
+        "sql": "SELECT * FROM submissionInfo." + classID + " WHERE assNo = " + assNo,
+    });
+
+    var response = await sendRequest.sendRequest(data).catch(e => console.log(e));
+
+    return response.data;
+}
+
+async function getDeadline(classID, assNo) {
+    classID = "id_" + classID;
+
+    var data = JSON.stringify({
+        "operation": "sql",
+        "sql": "SELECT * FROM assignmentInfo." + classID + " WHERE assNo = " + assNo,
+    });
+
+    var response = await sendRequest.sendRequest(data).catch(e => console.log(e));
+
+    return parseInt((response.data)[0].deadline.slice(5));
+}
+
+async function getStudents(classID) {
+    classID = "id_" + classID;
+
+    var data = JSON.stringify({
+        "operation": "sql",
+        "sql": "SELECT userID FROM userInfo." + classID,
+    });
+
+    var response = await sendRequest.sendRequest(data).catch(e => console.log(e));
+
+    return response;
+}
+
+async function handleReport(message, assNo) {
+    var classID = message.channel.id;
+
+    var submissions = await getReport(assNo, classID);
+    submissions.sort((a, b) => a.__updatedtime__ - b.__updatedtime__);
+
+    var user = "";
+    var submission = "";
+    var status = "";
+
+    var deadline = await getDeadline(classID, assNo);
+
+    for (var i = 0; i < submissions.length; i++) {
+        user = user + "<@" + submissions[i].userID.slice(3) + ">\n";
+
+        submission = submission + "[Link](" + submissions[i].url + ")\n";
+
+        var submissionTime = submissions[i].__updatedtime__;
+
+        if (submissionTime > deadline) {
+            status = status + "OK\n";
+        }
+        else {
+            status = status + "LATE\n";
+        }
+    }
+
+    message.channel.send({
+        embed: {
+            color: 3447003,
+            title: "Report of Assignment No. " + assNo,
+            fields: [
+                { name: "User", value: user, inline: true },
+                { name: "URL", value: submission, inline: true },
+                { name: "Status", value: status, inline: true }
+            ]
+        }
+    });
+}
+
+async function handleNotReport(message, assNo) {
+
+    var classID = message.channel.id;
+
+    var response = await getReport(assNo, classID);
+
+    var submitted = {};
+
+    for (var i = 0; i < response.length; i++) {
+        submitted[response[i].userID] = true;
+    }
+
+    var students = await getStudents(classID);
+
+    students = students.data;
+
+    var absent = "";
+
+    for (var i = 0; i < students.length; i++) {
+        if (submitted[students[i].userID] === undefined) {
+            absent = "<@" + students[i].userID.slice(3) + ">\n";
+        }
+    }
+
+    if(absent===""){
+        message.reply("Everyone has submitted the Assignment :slight_smile:");
+        return ;
+    }
+
+    message.channel.send({
+        embed: {
+            color: 3447003,
+            title: "Not Submitted Assignment No. " + assNo,
+            fields: [
+                { name: "User", value: absent, inline: true }
+            ]
+        }
+    });
+}
+
+async function handleView(message) {
+
+    var commandBody = message.content.slice(prefix.length);
+    var args = commandBody.split(' ');
+    args.shift();
+
+    if (args.length === 0) {
+        await handleAssignments(message);
+        return;
+    }
+
+    if (args.length != 2) {
+        message.reply("Incorrect Command Format");
+        return;
+    }
+
+    if (args[1] != "report") {
+        message.reply("Incorrect Command Format");
+        return;
+    }
+
+    var assNo;
+    var flip = false;
+
+    if (args[0][0] === '!') {
+        flip = true;
+        args[0] = args[0].slice(1);
+    }
+
+    try {
+        assNo = parseInt(args[0], 10);
+    }
+    catch (err) {
+        message.reply("Incorrect Command Format");
+        return;
+    }
+
+    if (assNo < 1) {
+        message.reply("Invalid Assignment Number");
+        return;
+    }
+
+    if (flip) {
+        await handleNotReport(message, assNo);
+    } else {
+        await handleReport(message, assNo);
+    }
 }
 
 module.exports = { handleView };
